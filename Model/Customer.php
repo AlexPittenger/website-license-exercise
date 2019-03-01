64592d689b76c7e872f623f14e06658d55bddf6a
Alex Pittenger <alex.pittenger@gmail.com>
2019-03-01 16:13:56 -0500

    Create Customer.php

---
+++ Model/Customer.php
@@ -0,0 +1,40 @@
+<?php
+
+class Customer{
+    public $id;
+    public $email;    
+    public $name;
+    public $registered;
+    public $status;
+    public $groupId;
+    
+    public function __construct($id, $email, $name, $registered, $status = null, $groupId, $displayName = ''){
+        $this->id = $id;        
+        $this->email = $email;        
+        $this->name = $name;
+        $this->registered = $registered;
+        $this->status = $status;
+        $this->groupId = $groupId;
+        if($displayName) {
+            $this->displayName = $displayName;
+        } else {
+            $this->displayName = substr($name, 0, 14);
+        }
+
+    }
+    
+    public function isNew(){
+        try{
+            if($this->id == null){
+                return true;
+                echo "true";
+            }else{
+                return false;
+                echo "false";
+            }   
+        }catch(Exception $e){
+            error_log("Error - trackknack_" . $_SESSION['db'] . " - isNew(): ". print_r($e, true));
+        }
+    }
+    
+}
