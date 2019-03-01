0a9317ea278c2d9da567d6f30dafe22e4b7d18e8
Alex Pittenger <alex.pittenger@gmail.com>
2019-03-01 15:51:40 -0500

    Create router.php
    Initial commit.

---
+++ router.php
@@ -0,0 +1,42 @@
+<?php
+
+if (!isset($_SESSION['userID'])){
+    $action='LoginForm';
+    $params = array();
+}else{    
+    $queryString = $_SERVER['QUERY_STRING'];
+
+    $params = array();
+    $parsedQueryString = explode('&', $queryString);
+
+    if ($queryString){
+
+        foreach($parsedQueryString AS $parameter){
+            list($variable, $value) = explode('=', $parameter);
+            $params[$variable] = $value;
+        }
+
+        $action = $params['action'];
+    }else{
+        $action = 'Main';
+    }
+    
+    $db = getDB();
+    
+}
+
+
+$controllerClassName = $action . "Controller";
+
+$controllerFile = "controller/$className.php";
+
+if (file_exists($controllerFile)) {
+
+    require_once($controllerFile);   
+    
+    $controller = new $className;
+    $controller->main($params);
+} else {
+    die("The page was not found.");
+}
+
